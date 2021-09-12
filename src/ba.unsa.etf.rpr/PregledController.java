73bcbcb9abcb9f5789619886f2ac2f5410c5c6b8
Amna Jusić <ajusic5@etf.unsa.ba>
2021-09-12 17:14:29 +0200

    implementirana metoda okClick u PregledController

--- src/ba.unsa.etf.rpr/PregledController.java
+++ src/ba.unsa.etf.rpr/PregledController.java
@@ -3,13 +3,12 @@ package ba.unsa.etf.rpr;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DateCell;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -58,6 +57,32 @@ public class PregledController {
     }
 
     public Pregled okClick(ActionEvent actionEvent){
-        return null;
+        if(vrstaPregledaCB.getValue() == null || vrijemePregledaCB.getValue() == null || datumPregledaDP.getValue() == null) return null;
+
+        var splitString = vrijemePregledaCB.getSelectionModel().getSelectedItem().split(":");
+        var vrstaPregleda = vrstaPregledaCB.getValue();
+        var datumPregleda = datumPregledaDP.getValue().toString() + "T" + vrijemePregledaCB.getValue();
+
+        var nijeZauzet = dao.provjeriPregled(vrstaPregleda, datumPregleda);
+
+        if(!nijeZauzet){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Greška");
+            alert.setHeaderText("Neispravni podaci");
+            alert.setContentText("Već je zauzet ovaj termin za sve ljekare koji nude ovu uslugu!");
+            alert.setResizable(true);
+            alert.show();
+            return null;
+        }
+
+        pregled.setVrstaPregleda(vrstaPregledaCB.getValue());
+        pregled.setVrijemeZakazivanjaTermina(LocalDateTime.now());
+        pregled.setDatumIVrijemePregleda(LocalDateTime.of(datumPregledaDP.getValue(), LocalTime.of(Integer.parseInt(splitString[0]),Integer.parseInt(splitString[1]))));
+        pregled.setVrijemeZakazivanjaTermina(LocalDateTime.now());
+        pregled.setPacijent(pacijent);
+        dao.dodajPregled(pregled);
+        Stage stage = (Stage) okBtn.getScene().getWindow();
+        stage.close();
+        return pregled;
     }
 }
