634e2ed41bec311dbad3f40500db48852e155e5c
Amna Jusić <ajusic5@etf.unsa.ba>
2021-09-12 23:25:03 +0200

    Dodan novi test konstruktora za Doctor klasu

--- test/ba/unsa/etf/rpr/DoctorTest.java
+++ test/ba/unsa/etf/rpr/DoctorTest.java
@@ -2,7 +2,9 @@ package ba.unsa.etf.rpr;
 
 import org.junit.jupiter.api.Test;
 
+import javax.print.Doc;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,5 +16,16 @@ class DoctorTest {
 
         assertEquals(1, doctor.getId());
         assertNull(doctor.getServices());
+    }
+
+    @Test
+    void doctorConstructorTest2(){
+        ArrayList<String> services = new ArrayList<>();
+        services.add("Ultrazvuk srca");
+
+        Doctor doctor = new Doctor(1, "Hanić", "Hana", LocalDate.of(1999, 9, 7), "hhanic1", "hana", ProfileType.LJEKAR, SexOfAUser.ZENSKI, "Oftamologija", services);
+
+        assertEquals("Hanić", doctor.getSurname());
+        assertNotNull(doctor.getServices());
     }
 }
