e20b2411896c1ae9a9fa7b83917a08790f9fe4a5
Amna Jusić <ajusic5@etf.unsa.ba>
2021-09-12 15:15:00 +0200

    kreirana klasa Pacijent izvedena iz klase Korisnik

---
+++ src/ba.unsa.etf.rpr/Pacijent.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr;
+
+import java.time.LocalDate;
+
+public class Pacijent extends Korisnik{
+
+    private int brojKartona;
+
+    public Pacijent() {
+    }
+
+    public Pacijent(int id, String prezime, String ime, LocalDate datumRodjenja, String username, String password, String vrsta, String spol, int brojKartona) {
+        super(id, prezime, ime, datumRodjenja, username, password, vrsta, spol);
+        this.brojKartona = brojKartona;
+    }
+
+    public int getBrojKartona() {
+        return brojKartona;
+    }
+
+    public void setBrojKartona(int brojKartona) {
+        this.brojKartona = brojKartona;
+    }
+}
